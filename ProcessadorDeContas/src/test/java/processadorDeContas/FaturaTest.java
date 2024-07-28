b57e8cdffe9317f955f83125469a35ab150452e5
Amauri Lima <amauri.plimaj@gmail.com>
2024-07-28 20:18:07 -0300

    test: writes test for fatura test initialization

---
+++ ProcessadorDeContas/src/test/java/processadorDeContas/FaturaTest.java
@@ -0,0 +1,14 @@
+package processadorDeContas;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.time.LocalDate;
+import org.junit.jupiter.api.Test;
+
+public class FaturaTest {
+  @Test
+  public void testFaturaCreation() {
+    Fatura fatura = new Fatura(LocalDate.now(), 1500.00, "Cliente A");
+    assertEquals(1500.00, fatura.getValorTotal());
+    assertEquals("Cliente A", fatura.getNomeCliente());
+  }
+}
