9b741eef7864510d6ad4248a11fe1a5c9fecdb85
Amauri Lima <amauri.plimaj@gmail.com>
2024-07-28 20:27:18 -0300

    feat: implements conta class to pass fatura test

---
+++ ProcessadorDeContas/src/main/java/processadorDeContas/Conta.java
@@ -0,0 +1,39 @@
+package processadorDeContas;
+
+import java.time.LocalDate;
+
+public class Conta {
+  private String codigoConta;
+  private LocalDate data;
+  private double valorPago;
+
+  public Conta(String codigoConta, LocalDate data, double valorPago) {
+    this.codigoConta = codigoConta;
+    this.data = data;
+    this.valorPago = valorPago;
+  }
+
+  public String getCodigoConta() {
+    return codigoConta;
+  }
+
+  public void setCodigoConta(String codigoConta) {
+    this.codigoConta = codigoConta;
+  }
+
+  public LocalDate getData() {
+    return data;
+  }
+
+  public void setData(LocalDate data) {
+    this.data = data;
+  }
+
+  public double getValorPago() {
+    return valorPago;
+  }
+
+  public void setValorPago(double valorPago) {
+    this.valorPago = valorPago;
+  }
+}
