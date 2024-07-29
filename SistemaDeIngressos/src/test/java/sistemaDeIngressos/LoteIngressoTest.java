f47dcf48bd12b889e7b1fbc8a5a064468b93a0eb
huandrey <huandrey.pontes@ccc.ufcg.edu.br>
2024-07-28 22:35:14 -0300

    🧪 Adiciona mais testes para classes básicas do sistema
    Adiciona teste para a classe Show e LoteIngresso; Testes feitos de acordo com as especificações iniciais do sistema;

--- SistemaDeIngressos/src/test/java/sistemaDeIngressos/LoteIngressoTest.java
+++ SistemaDeIngressos/src/test/java/sistemaDeIngressos/LoteIngressoTest.java
@@ -1,6 +1,12 @@
 package sistemaDeIngressos;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import sistemaDeIngressos.models.LoteIngresso;
 
@@ -10,5 +16,22 @@ public class LoteIngressoTest {
   @BeforeEach
   void setup() {
     loteIngresso = new LoteIngresso(1, 500, 10.00, 10.00, 20);
+  }
+
+  @Test
+  void recuperaInformacoesDoLote () {
+    assertNotNull(loteIngresso, "O objeto loteIngresso não deveria ser nulo.");
+    assertEquals(1, loteIngresso.getId(), "O id do lote de ingressos deve ser 1.");
+    assertEquals(500, loteIngresso.getQuantidadeIngresso(), "A quantidade de ingressos deve ser 500.");
+    assertEquals(10.00, loteIngresso.getPrecoNormalIngresso(), "O preço normal dos ingressos deve ser 10.00.");
+    assertEquals(10.00, loteIngresso.getDescontoAplicavel(), "O desconto aplicável dos ingressos deve ser 10.00.");
+  }
+
+  @Test
+  void criaLoteIngressoComDescontoMaiorQue25() {
+    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+      new LoteIngresso(1, 500, 10.0, 25.1, 20); // Desconto de 25.1%
+    }, "Esperava IllegalArgumentException para desconto acima de 25%.");
+    assertEquals("Desconto não pode ser maior que 25%.", exception.getMessage());
   }
 }
--- SistemaDeIngressos/src/test/java/sistemaDeIngressos/ShowTest.java
+++ SistemaDeIngressos/src/test/java/sistemaDeIngressos/ShowTest.java
@@ -1,7 +1,12 @@
 package sistemaDeIngressos;
 
-import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import sistemaDeIngressos.models.LoteIngresso;
 import sistemaDeIngressos.models.Show;
 
 public class ShowTest {
@@ -10,5 +15,14 @@ public class ShowTest {
   @BeforeEach
   void setup() {
     show = new Show(true, "Kendrick Lamar", 1000.00, 2000.00);
+  }
+
+  @Test
+  void criaLoteIngressoPorMeioDoShow() {
+    show.criaLoteIngresso(1, 500, 10.0, 15, 20);
+    LoteIngresso loteIngresso = show.getLoteIngresso(0);
+
+    assertNotNull(loteIngresso, "O objeto loteIngresso não deveria ser nulo.");
+    assertEquals(500, loteIngresso.getTotalIngressos(), "O número total de ingressos deve ser 500.");
   }
 }
